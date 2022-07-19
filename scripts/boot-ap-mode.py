92e85f7ad17a0f136fb28f32892933d1c5b9c962
Marcelo Tournier <15373278+marcelotournier@users.noreply.github.com>
2022-07-19 00:25:10 -0400

    Create boot-ap-mode.py

---
+++ scripts/boot-ap-mode.py
@@ -0,0 +1,21 @@
+"""
+This file is executed on every boot
+(including wake-boot from deepsleep)
+"""
+import esp
+import webrepl
+import network
+
+
+# Deactivating debug mode
+esp.osdebug(None)
+
+# Starting webREPL
+webrepl.start()
+
+# Create AP during boot:
+SSID = "Micropython-ESP32"
+PASS = "123123"
+ap = network.WLAN(network.AP_IF)
+ap.active(True)
+ap.config(essid=SSID, password=PASS)
